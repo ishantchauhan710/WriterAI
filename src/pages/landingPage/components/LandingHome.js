3e43f5443deaf4ddd0789c1c1005eb8541b02817
ishantchauhan710 <ishantchauhan710@gmail.com>
2022-07-21 12:45:57 +0530

    Lottie Animation Added In Landing Page Right

--- src/pages/landingPage/components/LandingHome.js
+++ src/pages/landingPage/components/LandingHome.js
@@ -1,3 +1,5 @@
+import Lottie from "lottie-react";
+import landingPageAnimation from "../../../res/animations/landing_page_anim.json"
 import React from "react";
 
 export const LandingHome = () => {
@@ -11,17 +13,24 @@ export const LandingHome = () => {
           Use the Power of Artificial Intelligence to write high quality content
         </div>
         <div className="landing-page__home__left__text-secondary mv-10">
-          WriterAI is an AI based content writing tool that helps users easily write blogs and directly publish them to platforms like hashnode, medium, and dev.to! Just provide the AI with an overview of the topic
-          and you will get the results instantly. 
+          WriterAI is an AI based content writing tool that helps users easily
+          write blogs and directly publish them to platforms like hashnode,
+          medium, and dev.to! Just provide the AI with an overview of the topic
+          and you will get the results instantly.
         </div>
         <div className="landing-page__home__left__buttons">
-        <button className="writerai-button landing-page__home__left__button--primary mv-10">Try now</button>
-        <button className="writerai-button landing-page__home__left__button--secondary mv-10">Source Code</button>
-          
+          <button className="writerai-button landing-page__home__left__button--primary mv-10">
+            Try now
+          </button>
+          <button className="writerai-button landing-page__home__left__button--secondary mv-10">
+            Source Code
+          </button>
         </div>
       </div>
 
-      <div className="landing-page__home__right">Right</div>
+      <div className="landing-page__home__right">
+        <Lottie style={{width: "100%", height: "100%"}} animationData={landingPageAnimation} loop={true} />
+      </div>
     </div>
   );
 };
