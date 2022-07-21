116eeecd566fb2c7f9514d5546709878098ac58a
ishantchauhan710 <ishantchauhan710@gmail.com>
2022-07-21 19:40:26 +0530

    Shadows, Hovers and Transitions Added in Landing Page

--- src/pages/landingPage/LandingPage.js
+++ src/pages/landingPage/LandingPage.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { isUserLoggedIn } from "../../firebase/firebase";
 import AuthModal from "./components/AuthModal";
 import { LandingFeatures } from "./components/LandingFeatures";
+import { LandingFooter } from "./components/LandingFooter";
 import LandingHeader from "./components/LandingHeader";
 import { LandingHome } from "./components/LandingHome";
 import { LandingPoweredBy } from "./components/LandingPoweredBy";
@@ -16,6 +17,7 @@ export const LandingPage = () => {
       <LandingHome />
       <LandingFeatures />
       <LandingPoweredBy />
+      <LandingFooter />
     </>
   );
 };
---
+++ src/pages/landingPage/components/LandingFooter.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LandingFooter = () => {
+  return (
+    <div className="landing-page__footer">Made by Ishant and Vaibhav</div>
+  )
+}
