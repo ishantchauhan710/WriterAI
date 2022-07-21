f8acbd2051b2aa118b924904b1134a828d90094b
ishantchauhan710 <ishantchauhan710@gmail.com>
2022-07-21 21:32:28 +0530

    Side Menu Added

--- src/pages/landingPage/components/LandingHeader.js
+++ src/pages/landingPage/components/LandingHeader.js
@@ -1,10 +1,34 @@
-import React from "react";
-import LandingPageDrawer from "./LandingPageDrawer";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import LandingPageDrawer from './LandingPageDrawer'
+import { isUserLoggedIn } from "../../../firebase/firebase";
+import AuthModal from "./AuthModal";
 
 const LandingHeader = () => {
+
+  const [openAuthModal, setOpenAuthModal] = useState(false);
+  const [authTab, setAuthTab] = useState("1"); // 1: Login, 2: SignUp
+  const navigate = useNavigate();
+
+  const openLoginModal = () => {
+    setOpenAuthModal(true);
+    setAuthTab("1");
+  };
+
+  const openSignUpModal = () => {
+    setOpenAuthModal(true);
+    setAuthTab("2");
+  };
+
   return (
     <div className="landing-page__header">
-      <div className="landing-page__header__logo">WriterAI</div>
+      <div className="landing-page__header__logo">
+        <LandingPageDrawer
+          openLoginModal={openLoginModal}
+          openSignUpModal={openSignUpModal}
+        />
+        WriterAI
+      </div>
       <div className="landing-page__header__links">
         <ul>
           <li>
@@ -22,8 +46,12 @@ const LandingHeader = () => {
         </ul>
       </div>
       <div className="landing-page__header__buttons">
-        <button className="landing-page__header__button landing-page__header__button--secondary writerai-button">Login</button>
-        <button className="landing-page__header__button landing-page__header__button--primary writerai-button">Sign Up</button>
+        <button className="landing-page__header__button landing-page__header__button--secondary writerai-button">
+          Login
+        </button>
+        <button className="landing-page__header__button landing-page__header__button--primary writerai-button">
+          Sign Up
+        </button>
       </div>
     </div>
   );
