f8780b725adc9f3b137e8786a70d0311f6a33a63
ishantchauhan710 <ishantchauhan710@gmail.com>
2022-07-23 12:09:08 +0530

    Popup Menu Added in Project Tab

--- src/pages/homePage/components/ProjectMenuDialog.js
+++ src/pages/homePage/components/ProjectMenuDialog.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
-export const ProjectMenuDialog = () => {
+export const ProjectMenuDialog = ({ open, setOpen, pos }) => {
+  const handleMenuClick = () => {
+    setOpen(false);
+  };
+
+  const componentRef = useRef();
+
+  let click = 0;
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+    function handleClick(e) {
+      if (componentRef && componentRef.current) {
+        const ref = componentRef.current;
+        if (ref.style.display == "block" && !ref.contains(e.target)) {
+          click = click + 1;
+          if (click >= 2) {
+            setOpen(false);
+            click = 0;
+          }
+        }
+      }
+    }
+  }, []);
+
   return (
-    <div>ProjectMenuDialog</div>
-  )
-}
+    <div
+      ref={componentRef}
+      style={{
+        display: open === true ? "block" : "none",
+        left: pos.pageX,
+        top: pos.pageY,
+      }}
+      className="home-page__popup-menu__data"
+      onBlur={() => handleMenuClick()}
+    >
+      <a onClick={() => handleMenuClick()} href="#">
+        Share
+      </a>
+      <a onClick={() => handleMenuClick()} href="#">
+        Revoke Access
+      </a>
+      <a onClick={() => handleMenuClick()} href="#">
+        Delete
+      </a>
+      <a onClick={() => handleMenuClick()} href="#">
+        Close
+      </a>
+    </div>
+  );
+};
--- src/pages/homePage/components/ProjectTab.js
+++ src/pages/homePage/components/ProjectTab.js
@@ -2,16 +2,26 @@ import React, { useState } from "react";
 import { ProjectMenuDialog } from "./ProjectMenuDialog";
 
 export const ProjectTab = ({ projects, label }) => {
+
+  const [showPopupMenu,setShowPopupMenu] =  useState(false);
+  const [popupMenuPos,setPopupMenuPos] =  useState({});
+  
+
+  const openMenu = (pos) => {
+    setShowPopupMenu(true);
+    setPopupMenuPos(pos)
+  }
+
   return (
     <div className="home-page__tab-data">
       <div className="home-page__tab-data__section-label">{label}</div>
+      <ProjectMenuDialog open={showPopupMenu} setOpen={setShowPopupMenu} pos={popupMenuPos} />
       <div className="home-page__tab-data__new-card-container">
-        {projects.map((item) => (
-          <div className="home-page__tab-data__content-card">
+        {projects.map((item, index) => (
+          <div key={index} className="home-page__tab-data__content-card">
             <div className="home-page__tab-data__content-card__img">
               <img src={item.image} />
-              <ProjectMenuDialog />
-              <div className="home-page__tab-data__content-card__menu_button">
+              <div onClick={(e) => openMenu(e)} className="home-page__tab-data__content-card__menu_button">
                 <i className="material-icons">more_vert</i>
               </div>
             </div>
